d801808c3f0438ebb601828b2f702ff2da98aeb8
Nashan <z929346631@gmail.com>
2022-02-14 11:08:22 +0800

    月度累计值拆分

--- data_processing.py
+++ data_processing.py
@@ -9,27 +9,31 @@ def date_ym(date):
     return date
 
 
-def org_data(value1):
-    print(value1)
-
+def org_data(data):
+    values = data.values
+    if len(values) > 1:
+        if values[1] > values[0]:
+            return values[1] - values[0]
+        else:
+            return values[1]
+    else:
+        return values[0]
 
 
 # for root, Dir, file in os.walk(os.path.dirname(os.path.abspath(__file__))):
 for root, Dirs, files in os.walk('.\\data_month'):
     for file in files:
         if '.xlsx' in file:
-            with open(os.path.join(root, file), 'rb') as f:
-                df = pd.read_excel(f, index_col=0)
-                # print(date_ym('2019年9月'))
+            path = os.path.join(root, file)
+            df = pd.read_excel(path, index_col=0)
+            # print(date_ym('2019年9月'))
+            if '月' in df.index.values:
                 df.index = list(map(date_ym, df.index.values))
-                df = df.sort_index()
-                # print
-                for value in df.columns.values:
-                    if '累计值' in value:
-                        name = value.split('_累计值')[0]
-                        print(df[value].rolling(2))
-                        df[name] = df[value].rolling(2).apply(org_data)
-                print(df.columns.values)
-                # df = df.apply(lambda x: date_ym(x))
-                # print(df)
-                exit()
+            for value in df.columns.values:
+                if '累计值' in value:
+                    name = value.split('_累计值')[0]
+                    df[name] = df[value].rolling(2, min_periods=1).apply(org_data)
+            # df = df.sort_index(ascending=True)
+            # print(df)
+            df.to_excel(path)
+            # exit()
